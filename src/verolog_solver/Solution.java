02c6538d044990f8fe42169ae44ffa5bfc408630
Apache3 <tj.thomas.jeantet@gmail.com>
2017-06-12 13:42:55 +0200

    first commit

---
+++ src/verolog_solver/Solution.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package verolog_solver;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Apache
+ */
+public class Solution {
+    
+    private LinkedList<Trip> trips;
+    private int totalDistance;
+    private int totalCost;
+    
+    public Solution()
+    {
+        trips = new LinkedList<>();
+        totalDistance = 0;
+        totalCost = 0;
+    }
+    
+    public void addTrip(Trip trip)
+    {
+        trips.add(trip);
+        totalDistance += trip.getDistance();
+        totalCost += trip.getCost();
+    }
+
+    public LinkedList<Trip> getTrips() {
+        return trips;
+    }
+
+    public int getTotalDistance() {
+        return totalDistance;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+}
